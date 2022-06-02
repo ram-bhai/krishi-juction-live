3869ad77e4f473d6d3d0a095547638e5040469e3
Shriram Mishra <ram.techcare27@gmail.com>
2022-06-02 18:32:11 +0530

    changin server.js

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const express = require('express')
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/frontend'));
-app.get('/', function(req, res) { res.sendFile(path.join(__dirname + 'dist/frontend/index.html')); });
-app.use(function(req, res) { res.sendFile(path.join(__dirname + 'dist/frontend/index.html')); });
+app.get('/', function(req, res) {
+    console.log("/ root route...");
+    return res.sendFile(path.join(__dirname + 'dist/frontend/index.html'));
+});
+app.use(function(req, res) {
+    console.log("wild route..........");
+    return res.sendFile(path.join(__dirname + 'dist/frontend/index.html'));
+});
 app.listen(process.env.PORT || 8080);
 
 
