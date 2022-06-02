8edabacf203291f04e843af570dfa42847721708
Shriram Mishra <ram.techcare27@gmail.com>
2022-06-02 18:42:52 +0530

    server.js changed again

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const app = express();
 app.use(express.static(__dirname + '/dist/frontend'));
 app.get('/', function(req, res) {
     console.log("/ root route...");
-    return res.sendFile(path.join(__dirname + 'dist/frontend/index.html'));
+    return res.sendFile(path.join(__dirname + '/dist/frontend/index.html'));
 });
 app.use(function(req, res) {
     console.log("wild route..........");
-    return res.sendFile(path.join(__dirname + 'dist/frontend/index.html'));
+    return res.sendFile(path.join(__dirname + '/dist/frontend/index.html'));
 });
 app.listen(process.env.PORT || 8080);
 
