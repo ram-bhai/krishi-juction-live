5939cd0d017d83c98c49344567a34347cf343f78
26palash <95613328+26palash@users.noreply.github.com>
2022-06-19 17:18:34 +0530

    spinner

--- src/app/contract-lists/contract-lists.component.ts
+++ src/app/contract-lists/contract-lists.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ContractFarmingService } from '../service/contract-farming.service';
+import { UserService } from '../service/user.service';
 
 @Component({
   selector: 'app-contract-lists',
@@ -8,7 +9,14 @@ import { ContractFarmingService } from '../service/contract-farming.service';
 })
 export class ContractListsComponent implements OnInit {
 
-  constructor(private contractFarmingService: ContractFarmingService) { 
+  constructor(private contractFarmingService: ContractFarmingService,private userService: UserService) {
+    this.contractFarmingService.getReq()
+    .subscribe(data => {
+      console.log(data);
+      this.reqList = data;
+      
+    })
+    
     this.contractFarmingService.landlist()
     .subscribe(data =>{
       this.approvedList = data;
@@ -18,10 +26,37 @@ export class ContractListsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  count=0;
+  reqList:any;
   list:any='';
   public approvedList:any[]=[];
 
   getDetail(list:any){
     this.list = list;
+  }
+
+
+
+  checkreq():boolean{
+    for(var i=0;i<this.reqList.length;i++){
+      if(this.reqList[i].userId == sessionStorage.getItem("id"))
+        return false;
+      if(this.count == 1)
+        return false;
+    }
+    return true;
+  }
+
+  bookreq(id:any){
+    var uid = this.userService.checkToken();
+    if(uid){
+      console.log("user id "+uid);
+      console.log("contract id "+id);
+      this.contractFarmingService.getlandreq(sessionStorage.getItem('id'),id)
+      .subscribe(data =>{
+        console.log(data);
+        this.count = 1;
+      })
+    }
   }
 }
--- src/app/service/contract-farming.service.ts
+++ src/app/service/contract-farming.service.ts
@@ -9,8 +9,18 @@ export class ContractFarmingService {
 
   constructor(private http : HttpClient) { }
   landList = 'https://krishi-backend.herokuapp.com/contract/view-requests';
+  landreq = "http://localhost:3000/farmrequest/deal/";
+  getreq = "http://localhost:3000/farmrequest/book-list/";
 
   landlist(): Observable<any>{
     return this.http.get(this.landList);
+  }
+
+  getlandreq(uid:any,cid:any): Observable<any>{
+    return this.http.post(this.landreq+uid+"/"+cid,{});
+  }
+
+  getReq():Observable<any>{
+    return this.http.get(this.getreq+sessionStorage.getItem('id'));
   }
 }
